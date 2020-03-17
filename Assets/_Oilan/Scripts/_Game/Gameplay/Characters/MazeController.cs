2c963bfe32f71e12f90d402af96096d95902bc7c
Sanat Seidgaparov <s.seidgaparov@gmail.com>
2020-03-17 17:32:00 +0500

    Revert "Merge branch 'master' of https://github.com/pickuperast/Oilan-we"
    This reverts commit b604883cc34c6608712eb2753616f12882c06178, reversing
    changes made to 0290a11d24bcd7b1210af4dc51b9d06830cae111.

---
+++ Assets/_Oilan/Scripts/_Game/Gameplay/Characters/MazeController.cs
@@ -0,0 +1,33 @@
+﻿using Oilan;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Oilan
+{
+
+    public class MazeController : MonoBehaviour
+    {
+        public float speed = 10f;
+        Rigidbody2D rb;
+        Vector2 movement;
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
+
+        private void FixedUpdate()
+        {
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        }
+    }
+}
