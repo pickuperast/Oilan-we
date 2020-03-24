00038a9284285904d56781798346f1c6ddc477fc
Евгений <37345286+virusdogs111@users.noreply.github.com>
2020-03-24 16:02:25 +0600

    fix letter and add last 3 step timeline

---
+++ Assets/_Oilan/Scripts/_Game/Gameplay/Quests/L1S3_Lab/MazeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeController : MonoBehaviour
+{
+    public float speed = 10f;
+    Rigidbody2D rb;
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
