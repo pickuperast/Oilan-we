a738fb59c488da786c2aecdad24a2cf515db260d
pickuperast <s.seidgaparov@gmail.com>
2020-04-02 17:11:16 +0600

    walk bool anim

--- Assets/_Oilan/Scripts/_Game/Gameplay/Characters/Character_Ali.cs
+++ Assets/_Oilan/Scripts/_Game/Gameplay/Characters/Character_Ali.cs
@@ -208,6 +208,10 @@ namespace Oilan
         {
             SetAnimatorBool("ali_eyes_front", isOn);
         }
+        public void SetAnimatorAli_Walk_Bool(bool isOn)
+        {
+            SetAnimatorBool("Walk", isOn);
+        }
         public void SetAnimatorAli_r83_Bool(bool isOn)
         {
             SetAnimatorBool("ali_r83_ukazyvaet_vpered", isOn);
