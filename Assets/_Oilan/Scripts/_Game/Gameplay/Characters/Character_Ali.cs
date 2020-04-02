d583f3d9036937533f2232a6ac95a9cd0a54df56
pickuperast <s.seidgaparov@gmail.com>
2020-04-02 20:02:21 +0600

    Revert "L1S2"
    This reverts commit fab50ea1c9ad28f668ef1963249d844763a97bae.

--- Assets/_Oilan/Scripts/_Game/Gameplay/Characters/Character_Ali.cs
+++ Assets/_Oilan/Scripts/_Game/Gameplay/Characters/Character_Ali.cs
@@ -51,7 +51,6 @@ namespace Oilan
         private float _equipment_Value = 0f;
         public float hold_Value = 0f;
         private float _hold_Value = 0f;
-        public float PushStrength = 10f;
         public bool isBeingPushed = false;
         private bool isRequiredToResetPush = true;
 
