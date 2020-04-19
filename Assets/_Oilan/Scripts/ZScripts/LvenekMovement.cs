8fc491c0d1f857d23715302757d638316c00741c
Go-Hard <35060710+Go-Hard@users.noreply.github.com>
2020-04-19 22:44:02 +0600

    add some things

---
+++ Assets/_Oilan/Scripts/ZScripts/LvenekMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LvenekMovement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
+    }
+}
