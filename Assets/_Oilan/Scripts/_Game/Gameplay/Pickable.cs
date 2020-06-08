41f007ccf4ac49f3c6f55ddad61d712ae4f85820
pickuperast <s.seidgaparov@gmail.com>
2020-06-08 21:45:54 +0500

    20200608

--- Assets/_Oilan/Scripts/_Game/Gameplay/Pickable.cs
+++ Assets/_Oilan/Scripts/_Game/Gameplay/Pickable.cs
@@ -21,14 +21,19 @@ namespace Oilan
             isPicked = false;
         }
 
+        private void Start()
+        {
+            if (SaveGameManager.Instance.mSaveData.level > GameplayManager.Instance.next_level_num) Destroy(gameObject);
+            if (SaveGameManager.Instance.mSaveData.step >= GameplayManager.Instance.next_step_num) Destroy(gameObject);
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("Player") && pickableType == PickableType.STAR && !isPicked)
             {
                 isPicked = true;
-                WebGLMessageHandler.Instance.AddWebsiteStar();
+                GameplayScoreManager.Instance.AddWebStars();
                 AudioManager.Instance.PlaySound("Zv-9 (Волшебный звук для звезды (отлетают на табло в меню “Награды”))");
-
                 Destroy(this.gameObject);
             }
         }
