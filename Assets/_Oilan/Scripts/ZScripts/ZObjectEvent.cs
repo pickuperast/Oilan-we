7fdc66dd1ae75963cea57f3eec30840a3d33a60d
Go-Hard <35060710+Go-Hard@users.noreply.github.com>
2020-09-28 16:22:23 +0600

    step 6

---
+++ Assets/_Oilan/Scripts/ZScripts/ZObjectEvent.cs
@@ -0,0 +1,25 @@
+﻿using Oilan;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZObjectEvent : MonoBehaviour
+{
+    public DragDropObject dropObject;
+    void Start()
+    {
+        dropObject.OnPlaced += DropObject;
+    }
+
+    // Update is called once per frame
+    void DropObject()
+    {
+        StartCoroutine(DropObjectCoroutine());
+    }
+
+    private IEnumerator DropObjectCoroutine()
+    {
+        GameplayTimelineManager.Instance.PlayNextTimeline();
+        yield return null;
+    }
+}
