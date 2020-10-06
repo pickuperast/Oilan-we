2e78abfd4b092981dc3c8efd2553d21cfb7356b9
pickuperast <s.seidgaparov@gmail.com>
2020-10-06 18:18:27 +0500

    clickable numbers v0.1

---
+++ Assets/_Oilan/Scripts/Sanat/SPressNumber.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SPressNumber : MonoBehaviour
+{
+    public int numberValue = 1;
+    public TMP_InputField SelectedField;
+
+    void Start()    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(CustomButton_onClick); //subscribe to the onClick event
+    }
+
+    //Handle the onClick event
+    void CustomButton_onClick()    {
+        Debug.Log(numberValue);
+    }
+
+    //Крепим поле на котором курсор через Unity сцену
+    public void SetSelectedField(TMP_InputField InSceneSelectedField)    {
+        SelectedField = InSceneSelectedField;
+    }
+}
