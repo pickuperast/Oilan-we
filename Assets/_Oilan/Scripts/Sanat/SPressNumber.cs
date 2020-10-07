a67dcb6913bd088f9d10c71162a8ccbac8359c39
pickuperast <s.seidgaparov@gmail.com>
2020-10-07 11:01:02 +0500

    test

--- Assets/_Oilan/Scripts/Sanat/SPressNumber.cs
+++ Assets/_Oilan/Scripts/Sanat/SPressNumber.cs
@@ -7,20 +7,21 @@ using TMPro;
 public class SPressNumber : MonoBehaviour
 {
     public int numberValue = 1;
-    public TMP_InputField SelectedField;
-
-    void Start()    {
-        Button btn = GetComponent<Button>();
-        btn.onClick.AddListener(CustomButton_onClick); //subscribe to the onClick event
-    }
+    public Button[] ButtonsNumbers;
 
     //Handle the onClick event
-    void CustomButton_onClick()    {
-        Debug.Log(numberValue);
+    void CustomButton_onClick(TMP_InputField SelectedField, int val)
+    {
+        SelectedField.text = val.ToString();
+        Debug.Log(val.ToString());
     }
 
     //Крепим поле на котором курсор через Unity сцену
-    public void SetSelectedField(TMP_InputField InSceneSelectedField)    {
-        SelectedField = InSceneSelectedField;
+    public void SetSelectedField(TMP_InputField InSceneSelectedField)
+    {
+        Debug.Log("selected");
+        for (int i = 0; i < ButtonsNumbers.Length; i++)        {
+            ButtonsNumbers[i].onClick.AddListener(() => CustomButton_onClick(InSceneSelectedField, i+1));
+        }
     }
 }
