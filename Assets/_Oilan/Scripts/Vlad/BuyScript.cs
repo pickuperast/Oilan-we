43fb4d2ba060d232ec7233691b3d01e759de8171
Copying-Ninja <2001vlad2908@gmail.com>
2020-10-12 16:59:17 +0300

    Shop UI

---
+++ Assets/_Oilan/Scripts/Vlad/BuyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject BuyButton;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IntemOnClick()
+    {
+        gameObject.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+        BuyButton.SetActive(true);
+    }
+    public void ItemDeselected()
+    {
+        gameObject.transform.localScale = new Vector3(1, 1, 1);
+        BuyButton.SetActive(false);
+    }
+}
---
+++ Assets/_Oilan/Scripts/Vlad/ShopScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ShopScript : MonoBehaviour
+{
+    public GameObject MapButton;
+    public GameObject ShopButton;
+
+    public void OpenShop()
+    {
+        //ShopButton.SetActive(false);
+        gameObject.SetActive(true);
+        MapButton.SetActive(false);
+        ShopButton.SetActive(false);
+    }
+    public void CloseShop()
+    {
+        gameObject.SetActive(false);
+        MapButton.SetActive(true); 
+        ShopButton.SetActive(true);
+    }
+
+}
