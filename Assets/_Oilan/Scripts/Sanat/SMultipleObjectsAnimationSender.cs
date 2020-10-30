7aab98d8bdc0a5f6a56959f47d53bd8266128f4b
pickuperast <s.seidgaparov@gmail.com>
2020-10-30 17:10:48 +0500

    tulpans

---
+++ Assets/_Oilan/Scripts/Sanat/SMultipleObjectsAnimationSender.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMultipleObjectsAnimationSender : MonoBehaviour
+{
+    public List<Animator> m_anim = new List<Animator>();
+
+    public void SetTrigger(string TriggerName)
+    {
+        foreach (var m_Animator in m_anim)
+            m_Animator.SetTrigger(TriggerName);
+    }
+
+    public void OnBool(string BoolName)
+    {
+        foreach (var m_Animator in m_anim)
+            m_Animator.SetBool(BoolName, true);
+    }
+
+    public void OffBool(string BoolName)
+    {
+        foreach (var m_Animator in m_anim)
+            m_Animator.SetBool(BoolName, false);
+    }
+}
